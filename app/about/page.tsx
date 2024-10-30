5808936236187388c7de2d3fc2188bb8a2863b4a
sahilyousaf2 <sahilyousaf66@gamil.com>
2024-10-30 16:02:46 +0500

    some change

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,15 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Image from "next/image";
+import Main from "../components/Main";
+
 
 export default function About() {
   return (
     <>
       <Header />
-      <main className="md:max-w-[1170px] max-w-[640px] mx-auto pt-8">
+      <Main />
+      {/* <main className="md:max-w-[1170px] max-w-[640px] mx-auto pt-8">
         <h1 className=" md:text-3xl text-xl text-center mb-8 font-bold">About</h1>
         <div className="grid md:grid-cols-[60%_auto] md:l-0">
           <div className=" md:p-6 pl-44 h-[440px]">
@@ -20,7 +23,7 @@ export default function About() {
             <Image className='mx-auto rounded-md hover:scale-105 duration-300 hover:shadow-2xl' src={"/men.jpg"} height={100} width={300} alt='men'></Image>
           </div>
         </div>
-      </main>
+      </main> */}
       <Footer />
     </>
   );
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,6 +7,43 @@ function Contact() {
     <>
       <Header />
       <div>
+        <main className="md:max-w-7xl max-w-[1000px] ml-[90px] mx-auto pt-20">
+          <div className="flex flex-col md:flex-row justify-center mb-20">
+            <div className="w-full md:w-1/2 p-6">
+              <form className="mt-4 shadow-2xl rounded-lg p-5">
+                <h1 className="text-3xl font-bold mb-2 text-center p-4">Contact Me</h1>
+                <div className="flex flex-col mb-4">
+                  <label htmlFor="name">Name:</label>
+                  <input
+                    type="text"
+                    id="name"
+                    className="w-full py-2 pl-10 text-sm rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 border"
+                  />
+                </div>
+                <div className="flex flex-col mb-4">
+                  <label htmlFor="email">Email:</label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="w-full py-2 pl-10 text-sm rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 border"
+                  />
+                </div>
+                <div className="flex flex-col mb-4">
+                  <label htmlFor="message">Message:</label>
+                  <textarea
+                    id="message"
+                    className="w-full py-2 pl-10 text-sm rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 resize-none border"
+                  />
+                </div>
+                <center>
+                  <Button size={"lg"} className="mt-4 hover:text-[#01AECD]" variant="outline">Submit</Button>
+                </center>
+              </form>
+            </div>
+          </div>
+        </main>
+      </div>
+      {/* <div>
 
 
         <main className="md:max-w-7xl max-w-[640px] mx-auto pt-20">
@@ -48,7 +85,7 @@ function Contact() {
           </div>
         </main>
 
-      </div>
+      </div> */}
       <Footer />
     </>
   );
